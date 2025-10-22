8cab1161946917c9423be2f930b1cefb06f2068d
nguyenanthachva59 <thaihanh14@gmail.com>
2025-10-22 22:17:57 +0700

    First commit

---
+++ scripts.js
@@ -0,0 +1,16 @@
+
+tailwind.config = {
+    darkMode: 'class',
+    theme: {
+        extend: {
+            colors: {
+                'primary': '#38bdf8',
+                'dark-bg': '#0f172a',
+                'dark-card': '#1e293b',
+            },
+            fontFamily: {
+                'sans': ['Inter', 'sans-serif']
+            },
+        }
+    }
+}
